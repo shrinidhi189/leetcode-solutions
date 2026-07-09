9bc8b657fe212ff54b75ae45bbea3753ced6c54c
T Shrinidhi <shrinidhithatta@gmail.com>
2026-07-09 18:38:53 +0530

    Create 1281. Subtract the Product and Sum of Digits of an Integer.java

---
+++ 1281. Subtract the Product and Sum of Digits of an Integer.java
@@ -0,0 +1,22 @@
+// 1281. Subtract the Product and Sum of Digits of an Integer
+// Difficulty: Easy
+// My Approach: While-loop
+// Time complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int subtractProductAndSum(int n) 
+    {
+        int sum=0;
+        int prod=1;
+        while(n!=0)
+        {
+            int r=n%10;
+            sum=sum+r;
+            prod=prod*r;
+            n=n/10;
+        }
+        return prod-sum;
+        
+    }
+}
